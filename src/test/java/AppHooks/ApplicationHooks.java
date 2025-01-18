21d2de5dc77f7d634de03df2d7efb3968b9b9bb7
MR.ROBOT <ballalatharvacr7@gmail.com>
2025-01-18 16:54:11 +0530

    Code update

--- src/test/java/AppHooks/ApplicationHooks.java
+++ src/test/java/AppHooks/ApplicationHooks.java
@@ -8,7 +8,6 @@ import io.cucumber.java.Scenario;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-
 import java.util.Properties;
 
 public class ApplicationHooks {
@@ -29,13 +28,12 @@ public class ApplicationHooks {
     @Before(order = 1)
     public void launchBrowser(){
         String browserName = prop.getProperty("browser");
-        String url = prop.getProperty("url");
         driverFactory = new DriverFactory();
         driver = driverFactory.init_driver(browserName);
-        driver.get(url);
 
     }
 
+    @Before(order = 2)
     public void getUrl(){
         String url = prop.getProperty("url");
         driver.get(url);
